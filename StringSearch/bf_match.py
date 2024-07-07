8521f9aa609d011957c343d23f24b71fd8743e4e
meticulousdev <meticulousdev@gmail.com>
2024-07-07 22:33:03 +0900

    main

--- StringSearch/bf_match.py
+++ StringSearch/bf_match.py
@@ -14,8 +14,15 @@ def bf_match(txt: str, pat: str) -> int:
 
 
 if __name__ == "__main__":
-    print("main")
+    s1 = "ADJNSABAISABCAJSDQNRMZPD"
+    s2 = "ABC"
 
+    idx = bf_match(s1, s2)
+
+    if idx == -1:
+        print("텍스트 안에 패턴이 존재하지 않습니다.")
+    else:
+        print(f"{idx+1}번째 문자가 일치합니다.")
 
 # Reference
 # Do it! 자료구조와 함께 배우는 알고리즘 입문: 파이썬 편
