b05c2e7f1dad925bdaea8d0cfd6225d7c9fce00c
meticulousdev <meticulousdev@gmail.com>
2024-08-04 23:12:45 +0900

    slicing using regex

--- PythonAlgorithmInterview/ch06/prob01.py
+++ PythonAlgorithmInterview/ch06/prob01.py
@@ -1,5 +1,7 @@
 import collections
 from typing import Deque
+import re
+
 
 class Solution:
     # soln01
@@ -27,13 +29,24 @@ class Solution:
                 return False
 
         return True
+    
+    # soln03
+    def isPalindrome_slicing(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub('[^a-z0-9]', '', s)
+
+        return s == s[::-1]  # 슬라이싱
 
 
 if __name__ == "__main__":
     str_example = "A man, a plan, a canal: Panama"
 
     soln = Solution()
-    print(soln.isPalindrome(str_example))
+    print(soln.isPalindrome_list(str_example))
+    print()
+    print(soln.isPalindrome_deque(str_example))
+    print()
+    print(soln.isPalindrome_slicing(str_example))
     print()
     
     # TEST
