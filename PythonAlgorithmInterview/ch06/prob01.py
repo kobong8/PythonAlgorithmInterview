4ea5eb5f7c772090e261055fd7f0a905ff10a93d
meticulousdev <meticulousdev@gmail.com>
2024-08-04 23:00:46 +0900

    Deque

--- PythonAlgorithmInterview/ch06/prob01.py
+++ PythonAlgorithmInterview/ch06/prob01.py
@@ -1,12 +1,29 @@
+import collections
+from typing import Deque
+
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        strs = []
+    # soln01
+    def isPalindrome_list(self, s: str) -> bool:
+        strs: list = []
         for char in s:
             if char.isalnum():
                 strs.append(char.lower())
 
         while len(strs) > 1:
             if strs.pop(0) != strs.pop():
+                return False
+
+        return True
+    
+    # soln02
+    def isPalindrome_deque(self, s: str) -> bool:
+        strs: Deque = collections.deque()
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
                 return False
 
         return True
