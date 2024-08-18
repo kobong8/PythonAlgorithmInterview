8eaaa92cd5e45a69959a9c55faf43fbc41e29f8e
meticulousdev <meticulousdev@gmail.com>
2024-08-18 22:34:08 +0900

    print update

--- PythonAlgorithmInterview/ch06/prob03_reorder_data_in_log_files.py
+++ PythonAlgorithmInterview/ch06/prob03_reorder_data_in_log_files.py
@@ -5,9 +5,9 @@ class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         letters, digits = [], []
         for log in logs:
-            print(log.split())
-            print(log.split()[1])
-            print()
+            # print(log.split())
+            # print(log.split()[1])
+            # print()
             if log.split()[1].isdigit():
                 digits.append(log)
             else:
