7c8d3d8565d75ba63665a967c49e913fef2f1b3f
meticulousdev <meticulousdev@gmail.com>
2024-08-25 14:31:36 +0900

    most common word

--- 01_PythonAlgorithmInterview/ch06/prob04_most_common_word.py
+++ 01_PythonAlgorithmInterview/ch06/prob04_most_common_word.py
@@ -1,4 +1,21 @@
+from typing import List
+import collections
+import re
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
+
+        counts = collections.Counter(words)
+        return counts.most_common(1)[0][0]
+
+
 if __name__ == "__main__":
     paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+    banned = ["hit"]
     print(paragraph.split())
+    
+    soln = Solution()
+    mcw = soln.mostCommonWord(paragraph, banned)
+    print(mcw)
     
