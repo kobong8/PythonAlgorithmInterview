c075178b0b8469da4427a9a53e663c6abc3f7b1b
kobong8 <fblsj90@gmail.com>
2025-05-01 22:37:32 +0900

    (soln) three points

--- ch07/prob09_sum_to_zero.py
+++ ch07/prob09_sum_to_zero.py
@@ -79,6 +79,38 @@ class Solution:
         print(f"cnt: {cnt}")
         return results
 
+    @timefn
+    def zero_sum_triplets_04(self, nums: list[int]) -> list[list[int]]:
+        print("zero_sum_triplets - (soln) three points")
+        results = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            # 중복된 값 건너뛰기
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            # 간격을 좁혀가며 합 `sum` 계산
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    # `sum = 0`인 경우이므로 정답 및 스킵 처리
+                    results.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+
+        return results
+
 
 # %%
 if __name__ == "__main__":
