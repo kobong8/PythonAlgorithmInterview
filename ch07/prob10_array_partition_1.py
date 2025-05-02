340e0f10154c7ad31fa4e17d13caa486536b5dca
kobong8 <fblsj90@gmail.com>
2025-05-02 12:57:05 +0900

    array partition

---
+++ ch07/prob10_array_partition_1.py
@@ -0,0 +1,16 @@
+from util import timefn
+
+
+# %%
+class Solution:
+    @timefn
+    def array_partition_01(self, nums: list[int]) -> int:
+        print("array_partition_01")
+
+        return 0
+
+
+# %%
+if __name__ == "__main__":
+
+    soln = Solution()
