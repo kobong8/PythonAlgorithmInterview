3453b3113beea04829761c4081da1e15266ce822
kobong8 <fblsj90@gmail.com>
2025-05-07 23:40:03 +0900

    soln 1 and 2

--- ch07/prob10_array_partition_01.py
+++ ch07/prob10_array_partition_01.py
@@ -7,12 +7,24 @@ class Solution:
     def array_partition_01(self, nums: list[int]) -> int:
         print("array_partition_01")
         nums.sort()
-        reps = len(nums) / 2
+        reps = int(len(nums) / 2)
         sum = 0
 
         for idx in range(reps):
-            min_value = min(nums[2 * idx - 1], nums[2 * idx])
+            min_value = min(nums[2 * idx], nums[2 * idx + 1])
             sum = sum + min_value
+
+        return sum
+
+    @timefn
+    def array_partition_02(self, nums: list[int]) -> int:
+        print("array_partition_02")
+        nums.sort()
+        reps = int(len(nums) / 2)
+        sum = 0
+
+        for idx in range(reps):
+            sum = sum + nums[2 * idx]
 
         return sum
 
@@ -22,3 +34,6 @@ if __name__ == "__main__":
     nums: list[int] = [1, 4, 3, 2]
     soln = Solution()
     print(soln.array_partition_01(nums))
+    print()
+    print(soln.array_partition_02(nums))
+    print()
